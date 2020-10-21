1bd203629e36912173f74beaf9ce16d534ec2884
Martin <martindaniels02@gmail.com>
2020-10-21 00:53:41 -0500

    logo redirect fix

--- youtube_mp3_site/errors/error_handlers.py
+++ youtube_mp3_site/errors/error_handlers.py
@@ -10,6 +10,6 @@ def error_403(error):
 def error_404(error):
     return render_template('errors/404.html'), 404
 
-@errors.app_errorhandler(403)
+@errors.app_errorhandler(500)
 def error_500(error):
     return render_template('errors/500.html'), 500
